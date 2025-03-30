f37d1605ad699dd9fe63c38ae7646f9f16dffc82
guikominami <contato.mudalab@gmail.com>
2025-03-30 18:55:45 +0200

    tutorials end

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -13,34 +13,33 @@ export default function Slide({ project }) {
   useEffect(() => {
     setShowDelayedImage(false);
     setTimeout(() => {
-      console.log("entrou");
       setShowDelayedImage(true);
     }, 100);
   }, [actualProject]);
 
   return (
-    <div className="slide">
-      <div className="content-img">
-        {showDelayedImage && <img src={project.img} alt="" />}
+    <div className='slide'>
+      <div className='content-img'>
+        {showDelayedImage && <img src={project.img} alt='' />}
       </div>
-      <div className="content-text">
+      <div className='content-text'>
         <h1>{project.title}</h1>
         <p>{project.descriptionEn}</p>
         <p>
-          <img className="icon" src={tools} alt="" />
+          <img className='icon' src={tools} alt='' />
           {project.techs.map((item, index) => (
             <span key={index}>
-              <button className="techs">{item}</button>
+              <button className='techs'>{item}</button>
             </span>
           ))}
         </p>
         <p>
-          <a href={project.link} target="_blank">
-            <img className="icon" src={link} alt="" />
+          <a href={project.link} target='_blank'>
+            <img className='icon' src={link} alt='' />
           </a>
           {project.code && (
-            <a href={project.code} target="blank">
-              <img className="icon" src={code} alt="" />
+            <a href={project.code} target='blank'>
+              <img className='icon' src={code} alt='' />
             </a>
           )}
         </p>
